ecd3e584abc1c6285dac61cd7f2cd0cc962c3ca4
Xnodes Development <xnodesdev@gmail.com>
2026-09-23 10:51:18 +0530

    Update broadcast and bot messages in example.js

--- example.js
+++ example.js
@@ -33,14 +33,14 @@ async function startZoom() {
 
   // Send message to all (broadcast)
   setTimeout(async () => {
-    await sock.sendMessage('all', { text: 'Hello from Baileys-style Zoom Engine!' });
+    await sock.sendMessage('all', { text: 'Hello !' });
   }, 10000);
 
   // Send message from a single bot only
   setTimeout(async () => {
     const bots = sock.getParticipants();
     if (bots.length > 0) {
-      await sock.sendMessage(bots[0].id, { text: 'I am the leader bot.' });
+      await sock.sendMessage(bots[0].id, { text: 'hello.' });
     }
   }, 15000);
 
